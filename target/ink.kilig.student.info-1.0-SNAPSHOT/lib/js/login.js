f246379a0a7edfaa28f1ea3d952ff1e72a1d4600
1291945816 <mac1291945816@163.com>
2020-05-03 22:55:33 +0800

    update

---
+++ target/ink.kilig.student.info-1.0-SNAPSHOT/lib/js/login.js
@@ -0,0 +1,8 @@
+$(function (){
+    $("img#checkcode").click(
+        function () {
+            var img=$('#checkcode').get(0);
+            img.src="/test/checkcode?"+new Date().getTime();
+})});
+
+
